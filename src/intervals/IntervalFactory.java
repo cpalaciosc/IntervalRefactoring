6fd15198b99f9681e8955cae6d6af8b4d70abd41
rodrigolopez <rodrigo@wime.com.mx>
2015-03-26 17:45:37 +0100

    Se encapsula el tipo de opening en la factoria.

--- src/intervals/IntervalFactory.java
+++ src/intervals/IntervalFactory.java
@@ -4,6 +4,17 @@ package intervals;
 public class IntervalFactory {
 
 	public static Interval getInterval(double minimum, double maximum, Opening opening) {
-		return new Interval(minimum, maximum, opening);
+		switch (opening){
+			case LEFT_OPENED:
+				return new LeftOpenedInterval(minimum, maximum);
+			case RIGHT_OPENED:
+				return new RightOpenedInterval(minimum, maximum);
+			case BOTH_OPENED:
+				return new BothOpenedInterval(minimum, maximum);
+			case UNOPENED:
+				return new UnOpenedInterval(minimum, maximum);
+			default:
+				throw new IllegalArgumentException("Wrong Opening parameter");
+		}	
 	}
 }
