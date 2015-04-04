272734ca05e4eec325e2ceb67162df03169befa2
rodrigolopez <rodrigo@wime.com.mx>
2015-04-04 10:23:21 +0200

    Se crea la clase Point junto con un enum de su tipo.

---
+++ src/intervals/Point.java
@@ -0,0 +1,18 @@
+package intervals;
+
+public class Point {
+	private double value;
+	private OpeningPointType type;
+	
+	public double getValue() {
+		return value;
+	}
+
+	public void setValue(double value) {
+		this.value = value;
+	}
+
+	public OpeningPointType getType() {
+		return type;
+	}
+}
